17fa43925f3fec7fa8a41d5a671ca1f68cf3de49
ashur-k <ashurkanwal@yahoo.com>
2020-04-08 16:30:02 +0000

    tabular data presented on page

--- assets/Javascript_files/script.js
+++ assets/Javascript_files/script.js
@@ -1,31 +1,59 @@
 const baseURL = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?";
 
-function getData(type, cb) {
-    var data = null;
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+function getData(type, cb){
+var data = null;
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(responseText);
-            cb(JSON.parse(this.responseText));
-        }
-    };
+/*xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});*/
 
-    xhr.open("GET", `${baseURL}country=${type}`);
-    xhr.setRequestHeader("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "e31173d748msh068eb1b2648d235p15ca87jsnc9dda1b88073");
+xhr.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+       cb(JSON.parse(this.responseText));
+    }
+}
+
+xhr.open("GET", `${baseURL}country=${type}`);
+xhr.setRequestHeader("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "e31173d748msh068eb1b2648d235p15ca87jsnc9dda1b88073");
+
+xhr.send(data);
+}
+
+function getTableHeaders(obj) {
+      var tableHeaders = [];
+
+        Object.keys(obj).forEach(function(key){
+                tableHeaders.push(`<td>${key}</td>`);
+            });
+            return `<tr>${tableHeaders}</tr>`;
 }
 
 function writeToDocument(type) {
-  
-
+    var tableRows = [];
+   var el = document.getElementById("data");
+   el.innerHTML = "";
     getData(type, function(data) {
-        data = data.data.covid19Stats;
 
-        data.forEach(function(item) {
-            console.log(item);
-            el.innerHTML += "<p>" + item.province + "</p>";
+        //console.dir(data);
+        data = data.data.covid19Stats;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item){
+            var dataRow = [];
+            Object.keys(item).forEach(function(key){
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 50);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+            //el.innerHTML += `<p> Confirmed Deaths:${item.confirmed}</p>`;
         });
+            el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        
     });
 }
