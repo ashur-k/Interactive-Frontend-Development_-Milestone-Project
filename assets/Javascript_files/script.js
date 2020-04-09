001c8d2b576e63a610667f8c203463b5b381abfe
ashur-k <ashurkanwal@yahoo.com>
2020-04-09 00:46:31 +0000

    simple user interface implmented

--- assets/Javascript_files/script.js
+++ assets/Javascript_files/script.js
@@ -57,3 +57,14 @@ function writeToDocument(type) {
         
     });
 }
+ /*window.onload = function getCountries(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+        }
+    };
+    xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+    xhr.send();
+}
+*/
